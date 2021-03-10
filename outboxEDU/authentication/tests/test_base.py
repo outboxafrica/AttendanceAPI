288d858566d9009f07c0ca51d18129a6ac03e57b
JENNY ZALWANGO <JENNYZLAWANGO12@gmail.com>
2021-03-10 09:42:31 +0300

    Add test base file

---
+++ outboxEDU/authentication/tests/test_base.py
@@ -0,0 +1,31 @@
+import json
+
+from rest_framework.test import APITestCase, APIClient
+from ..models import User
+
+
+class TestBase(APITestCase):
+  def setUp(self):
+    self.client  = APIClient()
+
+    self.userdata:{
+        "username":"little",
+        "email":"little@gmail.com",
+        "password":"little33"
+      }
+    self.userdata2 = {
+        "username":"little",
+        "email":"little@gmail.com",
+        "password":"little33"
+    }
+
+  # individualise user
+  username = "jennylittle234"
+  email = "jennylittle234@gmail.com"
+  password = "1234little"
+
+  # create user
+  saved_user = User(
+    username=username,
+    email=email,
+    password=password
